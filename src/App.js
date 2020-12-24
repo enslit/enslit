8eb4e2db34378a45bf41bcc432e366121360906e
Aleksandr Emelianov <alex@enslit.ru>
2020-12-24 15:23:15 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="message">
+        <h1 className="title">Сайт в разработке</h1>
+        <p className="text">Контакты для связи</p>
+        <p className="text">Мобильный: <a href="tel:+79685987856" className="link">+7 (968) 598-78-56</a></p>
+        <p className="text">Email: <a href="mailto:info@enslit.ru" className="link">info@enslit.ru</a></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
